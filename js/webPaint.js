5a9b3446d75eca886d560a8c62b104c411137cca
aammsst <Navarro.fa998@gmail.com>
2023-05-08 15:54:50 -0300

    moving from portfolio repo

--- js/webPaint.js
+++ js/webPaint.js
@@ -6,6 +6,7 @@ const ctxx = webPaint.getContext("2d");
 
 let painting,color,linewidth,difX,difY;
 
+// Mouse events {{{
 webPaint.addEventListener("mousedown", e => {
     difX = e.clientX - dif.left;
     difY = e.clientY - dif.top;
@@ -27,7 +28,38 @@ webPaint.addEventListener("mouseup",()=>{
     ctxx.closePath();
     painting = false;
 })
+// }}}
 
+// Touch events {{{
+
+webPaint.addEventListener("touchstart", e => {
+    e.preventDefault();
+    difX = e.changedTouches[0].clientX - dif.left;
+    difY = e.changedTouches[0].clientY - dif.top;
+    painting = true;
+    color = document.getElementById("color").value;
+    linewidth = document.getElementById("lw").value;
+    ctxx.beginPath();
+})
+
+webPaint.addEventListener("touchmove", e => {
+    e.preventDefault();
+    if (painting) {
+	dibujar(difX, difY, e.changedTouches[0].clientX - dif.left, e.changedTouches[0].clientY - dif.top);
+	difX = e.changedTouches[0].clientX - difX.left;
+	difY = e.changedTouches[0].clientY - difY.top;
+    }
+})
+
+webPaint.addEventListener("touchend", e => {
+    e.preventDefault();
+    ctxx.closePath();
+    painting = false;
+})
+
+// }}}
+
+// Draw {{{
 const dibujar = (x1,y1,x2,y2) => {
     ctxx.strokeStyle = color;
     ctxx.lineWidth = linewidth;
@@ -35,3 +67,4 @@ const dibujar = (x1,y1,x2,y2) => {
     ctxx.lineTo(x2,y2);
     ctxx.stroke();
 }
+// }}}
